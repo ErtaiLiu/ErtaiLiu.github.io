aef87d2e2dbb2dc1614d76bfb07ce2be139a8377
Your Name <you@example.com>
2023-12-01 16:27:01 -0600

    update IROS2023 post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "IROS2023 Presentation",
+        "excerpt":"October 5th, 2023 I am delighted to share that our research on visual navigation with automatic annotation has been accepted and presented at the IROS2023 conference in Detroit! I am sincerely grateful for the thoughtful questions and insightful comments I received both during and after the conference. If you have...","categories": [],
+        "tags": [],
+        "url": "/IROS2023/",
+        "teaser": null
+      },{
+        "title": "Fast Disease Detection Network",
+        "excerpt":"Pixel level segmentation has its unique advantage in image-based plant disease detection. The segmentation mask approximates the irregular shape of the infection in the image, making the data annotation and human verification straightforward. Moreover, the output mask correlates with the size of the observed infection, which can be linked to...","categories": [],
+        "tags": [],
+        "url": "/projects/2020-03-01-Fast-Disease-Detection/",
+        "teaser": "/assets/images/Disease_Detection/Teaser.png"
+      },{
+        "title": "PhytoPatholoBots (PPB)",
+        "excerpt":"This is the first version of my general robotic platform. The name “PhytoPatholoBots” represents its mission: it is a robot designed for plant pathology information gathering using proximal sensing and computer vision methods. Our recent findings indicate that strobe light enhanced camera is a critical tool for in-field stable image-based...","categories": [],
+        "tags": [],
+        "url": "/projects/2020-07-20-PPB/",
+        "teaser": "/assets/images/PPB/Teaser.png"
+      },{
+        "title": "Visual Navigation",
+        "excerpt":"This is an ongoing project. Please visit our most recent paper for details.   ","categories": [],
+        "tags": [],
+        "url": "/projects/2023-03-01-Visual-Navigation/",
+        "teaser": "/assets/images/Navigation/Teaser.png"
+      }]
