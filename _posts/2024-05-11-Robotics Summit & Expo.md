c5f372507b2f9e2e8ac492a00e9b8d5b6c7e4d6d
Your Name <you@example.com>
2024-05-11 22:13:26 -0500

    update

---
+++ _posts/2024-05-11-Robotics Summit & Expo.md
@@ -0,0 +1,20 @@
+---
+layout: single
+title: Robotics Summit & Expo recap
+---
+May 11th, 2024
+
+Several days ago, I had the pleasure of introducing our latest agricultural robotic innovation at the Form and Function Challenge during the Robotic Summit & Expo in Boston! Our new design prioritizes cost and energy efficiency while enhancing performance in navigating narrow and rugged agricultural terrain, particularly in vineyards. Using this concept, we constructed a compact 3D printed model car and were thrilled to witness its seamless execution of zero-point turns and Ackermann-like steering across diverse environments.
+
+
+The whole competition was very joyful, and we received much more than we expected. There are visotors stoping by our booths throughout the time, engaging with our presentation and offering invaluable suggestions. We forged connections with robotic component suppliers, like-minded companies, patent lawyers, and journalists, receiving insightful feedback on both our robotic design and business strategies. Additionally, we relished the opportunity of enjoying the exhibitions of the Robotics Summit & Expo, all at no cost!
+
+There is one technical lesson that we learned though. We wanted our small robot car to traverse in a rectangular patter on the booth by tracking the AprilTag. However we found that the motion blur of camera images made the tracking very unstable, partially due to the change of ambient light in the exhibition hall. Ideally we could have changed the tag patterns to a simpler, less informative but more robust one to overcome this issue, but unfortunately we were too late to find out this problem.
+
+If you have a robotic idea that you want to try out, I would definately encourage you to apply. The application is very easy. It is just a google form describing briefly about what's the purpose and the design concept of your robot. You will hear back from them within several days after the application deadline. You do need to specify how you would like to use the components from sponsors. This competition has sponsors from almost all aspects of robotics and they are all very easy to reach out and they are committed to help you! 
+
+I've also put together a short video capturing our experience at the event and our project. Should you wish to learn more about our journey or express interest in our robot, please don't hesitate to reach out. We're eager to share our experiences and insights with you!
+
+
+{% include video id="PyDf2UOSq90" provider="youtube" %}
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,21 +5,27 @@ var store = [{
         "url": "/IROS2023/",
         "teaser": null
       },{
+        "title": "Robotics Summit & Expo recap",
+        "excerpt":"May 11th, 2024 Several days ago, I had the pleasure of introducing our latest agricultural robotic innovation at the Form and Function Challenge during the Robotic Summit &amp; Expo in Boston! Our new design prioritizes cost and energy efficiency while enhancing performance in navigating narrow and rugged agricultural terrain, particularly...","categories": [],
+        "tags": [],
+        "url": "/Robotics-Summit-&-Expo/",
+        "teaser": null
+      },{
         "title": "Fast Disease Detection Network",
         "excerpt":"Pixel level segmentation has its unique advantage in image-based plant disease detection. The segmentation mask approximates the irregular shape of the infection in the image, making the data annotation and human verification straightforward. Moreover, the output mask correlates with the size of the observed infection, which can be linked to...","categories": [],
         "tags": [],
-        "url": "/projects/2020-03-01-Fast-Disease-Detection/",
+        "url": "/projects/2023-03-01-Fast-Disease-Detection/",
         "teaser": "/assets/images/Disease_Detection/Teaser.png"
-      },{
-        "title": "PhytoPatholoBots (PPB)",
-        "excerpt":"This is the first version of my general robotic platform. The name “PhytoPatholoBots” represents its mission: it is a robot designed for plant pathology information gathering using proximal sensing and computer vision methods. Our recent findings indicate that strobe light enhanced camera is a critical tool for in-field stable image-based...","categories": [],
-        "tags": [],
-        "url": "/projects/2020-07-20-PPB/",
-        "teaser": "/assets/images/PPB/Teaser.png"
       },{
         "title": "Visual Navigation",
         "excerpt":"This is an ongoing project. Please visit our most recent paper for details.   ","categories": [],
         "tags": [],
         "url": "/projects/2023-03-01-Visual-Navigation/",
         "teaser": "/assets/images/Navigation/Teaser.png"
+      },{
+        "title": "PhytoPatholoBots (PPB)",
+        "excerpt":"This is the first version of my general robotic platform. The name “PhytoPatholoBots” represents its mission: it is a robot designed for plant pathology information gathering using proximal sensing and computer vision methods. Our recent findings indicate that strobe light enhanced camera is a critical tool for in-field stable image-based...","categories": [],
+        "tags": [],
+        "url": "/projects/2024-03-19-PPB/",
+        "teaser": "/assets/images/PPB/Teaser.png"
       }]
